e8a8ff71aad970b3db451fe7d4cc2915cc8dfd3c
Alexander <alexander.antoschuk.dev@gmail.com>
2023-02-24 11:20:56 +0200

    initial

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import alias from '@rollup/plugin-alias'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+  alias({
+    entries: [
+      { find: '@', replacement: './src' },
+    ]
+  })],
+})
